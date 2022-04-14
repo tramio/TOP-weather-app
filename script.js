5a047ed7b502aae46d891b71108b51b43ae58312
Tramio <tramiodev@outlook.fr>
2022-04-14 23:16:02 +0200

    add styling, delete kelvinToCelsius function, add "units=metric" to API call

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const city = document.querySelector(".city");
+const general = document.querySelector(".general");
 const description = document.querySelector(".description");
+const submitCity = document.getElementById("submit-city");
 
-function kelvinToCelsius(kelvin) {
-  return parseInt(kelvin - 273.15);
+function meterPerSecondToKilometerPerHour(mps) {
+  return mps * 3.495;
 }
 
 const getForecast = (location) => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0982e2845899ef11e0c7aebc11449571`, {mode: 'cors'})
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0982e2845899ef11e0c7aebc11449571&units=metric`, {mode: 'cors'})
     .then((response) => {
       return response.json();
     })
     .then((response) => {
       city.textContent = response.name;
-      actualTemperature = kelvinToCelsius(response.main.temp);
-      feltTemperature = kelvinToCelsius(response.main.feels_like);
-      description.textContent = `At the present time the weather is ${response.weather[0].main.toLowerCase()}! The temperature is ${actualTemperature} °C but it feels ${feltTemperature} °C. The humidity is ${response.main.humidity}.`;
+      general.textContent = response.weather[0].main;
+      actualTemperature = parseInt(response.main.temp);
+      feltTemperature = parseInt(response.main.feels_like);
+      windSpeed = parseInt(meterPerSecondToKilometerPerHour(response.wind.speed));
+      description.textContent = `
+      Temperature: ${actualTemperature} °C |
+      Feels like: ${feltTemperature} °C |
+      Humidity: ${response.main.humidity} % |
+      Wind speed: ${windSpeed} km/h`;
     });
 }
-
 
 getForecast('Paris');
 
