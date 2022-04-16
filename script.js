2b96c6e7343537e7ac1d6dea5fa8c19d926f3f98
Tramio <tramiodev@outlook.fr>
2022-04-16 22:00:59 +0200

    add fetchForecast error handling

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const card = document.querySelector(".card");
 const city = document.querySelector(".city");
 const general = document.querySelector(".general");
 const description = document.querySelector(".description");
 const locationInput = document.getElementById("location-input");
 const locationSubmit = document.getElementById("location-submit");
+const forecast = Array.from(document.querySelectorAll(".forecast"));
 
 function meterPerSecondToKilometerPerHour(mps) {
   return mps * 3.495;
@@ -10,12 +12,30 @@ function meterPerSecondToKilometerPerHour(mps) {
 function getLocationInput() {
   return locationInput.value;
 }
+function displayErrorMessage() {
+  const h1 = document.createElement("h1");
+  const p = document.createElement("p");
+  const div = document.createElement("div");
+  div.setAttribute("id", "error-message");
+  h1.textContent = "Uh-oh";
+  p.textContent = `We couldn't find a location named "${locationInput.value}". Please check your spelling and try again. 🌞`;
+  div.appendChild(h1);
+  div.appendChild(p);
+  card.appendChild(div);
+}
+function clearErrorMessage() {
+  const errorMessage = document.querySelector("#error-message");
+  if (errorMessage) {
+    card.removeChild(errorMessage);
+  }
+}
 function fetchForecast(location) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0982e2845899ef11e0c7aebc11449571&units=metric`, {mode: 'cors'})
     .then((response) => {
       return response.json();
     })
     .then((response) => {
+      clearErrorMessage();
       city.textContent = response.name;
       general.textContent = response.weather[0].main;
       actualTemperature = parseInt(response.main.temp);
@@ -27,6 +47,10 @@ function fetchForecast(location) {
       Humidity: ${response.main.humidity} % |
       Wind speed: ${windSpeed} km/h`;
     })
+    .catch((error) => {
+      forecast.map((element) => {element.textContent = ""});
+      displayErrorMessage();
+    });
 }
 
 locationSubmit.addEventListener("click", () => {
